6c4cc52fa638a3794806f610d5ace14bdd344d09
Skrytty Kod <SkryttyKod@users.noreply.github.com>
2023-04-06 12:44:16 -0400

    Mi primer commit

---
+++ 01-variables.py
@@ -0,0 +1,31 @@
+#Un simple print
+print("Hola soy Matias")
+
+#Declarando una variable
+nombre = "Matias"
+name = "Daniel"
+
+#impresion de una variable
+print(name)
+print("Hola soy", name)
+print("Hola soy", nombre)
+
+#Declarando una tercera variable.
+edad = 28
+
+#Impresion de la tercera variable
+print ("Tengo", edad, "años de edad")
+
+###Imprimiendo 2 variables en una misma sentencia (concadenacion)###
+print("Hola mi nombre es " + nombre + " y tengo " + str(edad) + " años")
+
+#Actualizando la variable "nombre" (Mutable)
+nombre = "Cris"
+print("Hola mi nuevo nombre es", nombre)
+
+##Utilizando la instruccion input###
+nombre1 = input("¿Cual es tu nombre?\n")
+print("Tu nombre es", nombre1)
+
+
+

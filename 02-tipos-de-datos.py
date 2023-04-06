6f736b25ada49c9ac9c07f0b969d23af67a6a186
Skrytty Kod <SkryttyKod@users.noreply.github.com>
2023-04-06 15:58:44 -0400

    Tipos de datos

---
+++ 02-tipos-de-datos.py
@@ -0,0 +1,31 @@
+#01-DATOS DE TIPO NUMERICO
+estatura = 1.71
+peso = 72
+complejo = 1 +4j
+
+print("Impresion del numero complejo:", complejo)
+
+#OPERACION ARITMETICA
+imc = peso/estatura**2
+
+#PRINT IMC
+print("Mi indice de masa corporal es:",imc)
+
+#02-DATOS DE TIPO CADENA DE CARACTERES
+asignatura = "Programacion"
+carrera = "Ingenieria Civil en Informatica"
+
+print("La asignaruta de",asignatura,"corresponde a la carrera de",carrera)
+
+#03-VALORES BOLEANOS
+ampolleta = False
+interruptor = True
+
+#Con type sabemos el tipode  datos que estamos tratando
+print(type(ampolleta))
+
+#04-DATOS TIPO ARRAY (Objetos de Tipo Coleccion)
+estudiantes = ["Matias", "Marco", "Cristobal", "Sebastian"]
+num = [1,2,3,4,5,6]
+print(estudiantes)
+print(num)

46b6e17f64ef17b9b6963972ead008c86d9c915b
Skrytty Kod <SkryttyKod@users.noreply.github.com>
2023-05-03 17:57:47 -0400

    clase 03-05-2023 - Bucles

---
+++ 05-bucles.py
@@ -0,0 +1,20 @@
+edad = 15
+num = 0
+
+#while edad < 18:
+#    print('Eres menor de edad, no puedes manejar')
+
+# ¿Que hace este bucle?
+print('####### WHILE #######')
+while num <= 100:
+    print(num)
+    num = num + 2
+print('Primer bucle terminado\n')
+
+#Combinando while y else
+while num <= 200:
+    print(num)
+    num += 2
+else:
+    print('Mi condicion es igual o mayor a 200')
+print('Segundo bucle terminado.')
